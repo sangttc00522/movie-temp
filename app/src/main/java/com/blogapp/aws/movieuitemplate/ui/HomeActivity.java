a8f85be05c0efbae977b6dc9b0cd3e68c645e9b4
sangtt <sangtt@runsystem.net>
2019-11-20 11:52:44 +0700

    demo

--- app/src/main/java/com/blogapp/aws/movieuitemplate/ui/HomeActivity.java
+++ app/src/main/java/com/blogapp/aws/movieuitemplate/ui/HomeActivity.java
@@ -2,21 +2,23 @@ package com.blogapp.aws.movieuitemplate.ui;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.blogapp.aws.movieuitemplate.models.Movie;
+import com.blogapp.aws.movieuitemplate.R;
 import com.blogapp.aws.movieuitemplate.adapters.MovieAdapter;
 import com.blogapp.aws.movieuitemplate.adapters.MovieItemClickListener;
-import com.blogapp.aws.movieuitemplate.R;
-import com.blogapp.aws.movieuitemplate.models.Slide;
 import com.blogapp.aws.movieuitemplate.adapters.SliderPagerAdapter;
+import com.blogapp.aws.movieuitemplate.models.Movie;
+import com.blogapp.aws.movieuitemplate.models.Slide;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,75 +27,75 @@ import java.util.TimerTask;
 
 public class HomeActivity extends AppCompatActivity implements MovieItemClickListener {
 
-    private List<Slide> lstSlides ;
+    private List<Slide> lstSlides;
     private ViewPager sliderpager;
     private TabLayout indicator;
-    private RecyclerView MoviesRV ;
+    private RecyclerView MoviesRV1, MoviesRV2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        sliderpager = findViewById(R.id.slider_pager) ;
+        sliderpager = findViewById(R.id.slider_pager);
         indicator = findViewById(R.id.indicator);
-        MoviesRV = findViewById(R.id.Rv_movies);
+        MoviesRV1 = findViewById(R.id.Rv_movies1);
+        MoviesRV2 = findViewById(R.id.Rv_movies2);
 
         // prepare a list of slides ..
-        lstSlides = new ArrayList<>() ;
-        lstSlides.add(new Slide(R.drawable.slide1,"Slide Title \nmore text here"));
-        lstSlides.add(new Slide(R.drawable.slide2,"Slide Title \nmore text here"));
-        lstSlides.add(new Slide(R.drawable.slide1,"Slide Title \nmore text here"));
-        lstSlides.add(new Slide(R.drawable.slide2,"Slide Title \nmore text here"));
-        SliderPagerAdapter adapter = new SliderPagerAdapter(this,lstSlides);
+        lstSlides = new ArrayList<>();
+        lstSlides.add(new Slide(R.drawable.slide1, "Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide2, "Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide1, "Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide2, "Slide Title \nmore text here"));
+        SliderPagerAdapter adapter = new SliderPagerAdapter(this, lstSlides);
         sliderpager.setAdapter(adapter);
         // setup timer
         Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new HomeActivity.SliderTimer(),4000,6000);
-        indicator.setupWithViewPager(sliderpager,true);
+        timer.scheduleAtFixedRate(new HomeActivity.SliderTimer(), 4000, 6000);
+        indicator.setupWithViewPager(sliderpager, true);
 
         // Recyclerview Setup
         // ini data
 
         List<Movie> lstMovies = new ArrayList<>();
-        lstMovies.add(new Movie("Moana",R.drawable.moana,R.drawable.spidercover));
-        lstMovies.add(new Movie("Black P",R.drawable.blackp,R.drawable.spidercover));
-        lstMovies.add(new Movie("The Martian",R.drawable.themartian));
-        lstMovies.add(new Movie("The Martian",R.drawable.themartian));
-        lstMovies.add(new Movie("The Martian",R.drawable.themartian));
-        lstMovies.add(new Movie("The Martian",R.drawable.themartian));
+        lstMovies.add(new Movie("Moana", R.drawable.moana, R.drawable.spidercover));
+        lstMovies.add(new Movie("Black P", R.drawable.blackp, R.drawable.spidercover));
+        lstMovies.add(new Movie("The Martian", R.drawable.themartian));
+        lstMovies.add(new Movie("The Martian", R.drawable.themartian));
+        lstMovies.add(new Movie("The Martian", R.drawable.themartian));
+        lstMovies.add(new Movie("The Martian", R.drawable.themartian));
 
-        MovieAdapter movieAdapter = new MovieAdapter(this,lstMovies,this);
-        MoviesRV.setAdapter(movieAdapter);
-        MoviesRV.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
-
-
-
+        MovieAdapter movieAdapter = new MovieAdapter(this, lstMovies, this);
+        MoviesRV1.setAdapter(movieAdapter);
+        MoviesRV1.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        MoviesRV2.setAdapter(movieAdapter);
+        MoviesRV2.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onMovieClick(Movie movie, ImageView movieImageView) {
         // here we send movie information to detail activity
         // also we ll create the transition animation between the two activity
 
-        Intent intent = new Intent(this,MovieDetailActivity.class);
+        Intent intent = new Intent(this, MovieDetailActivity.class);
         // send movie information to deatilActivity
-        intent.putExtra("title",movie.getTitle());
-        intent.putExtra("imgURL",movie.getThumbnail());
-        intent.putExtra("imgCover",movie.getCoverPhoto());
+        intent.putExtra("title", movie.getTitle());
+        intent.putExtra("imgURL", movie.getThumbnail());
+        intent.putExtra("imgCover", movie.getCoverPhoto());
         // lets crezte the animation
         ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(HomeActivity.this,
-                                                    movieImageView,"sharedName");
+                movieImageView, "sharedName");
 
-        startActivity(intent,options.toBundle());
-
+        startActivity(intent, options.toBundle());
 
 
         // i l make a simple test to see if the click works
 
-        Toast.makeText(this,"item clicked : " + movie.getTitle(),Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "item clicked : " + movie.getTitle(), Toast.LENGTH_LONG).show();
         // it works great
 
 
@@ -108,10 +110,9 @@ public class HomeActivity extends AppCompatActivity implements MovieItemClickLis
             HomeActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    if (sliderpager.getCurrentItem()<lstSlides.size()-1) {
-                        sliderpager.setCurrentItem(sliderpager.getCurrentItem()+1);
-                    }
-                    else
+                    if (sliderpager.getCurrentItem() < lstSlides.size() - 1) {
+                        sliderpager.setCurrentItem(sliderpager.getCurrentItem() + 1);
+                    } else
                         sliderpager.setCurrentItem(0);
                 }
             });
@@ -119,9 +120,6 @@ public class HomeActivity extends AppCompatActivity implements MovieItemClickLis
 
         }
     }
-
-
-
 
 
 }
